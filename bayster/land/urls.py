a5dede05de7fff9dcec44339e9f9cbfe60afd1ea
ryota <ohashi@codor.co.jp>
2022-01-05 10:21:39 +0900

    making createview

--- bayster/land/urls.py
+++ bayster/land/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('land/', views.LandListView.as_view(), name='land-list'),
     path('detail/<int:pk>/', views.LandDetailView.as_view(), name='land-detail'),
+    path('create/', views.LandCreateView.as_view(), name='land-create'),
 ]
--- bayster/land/views.py
+++ bayster/land/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Land
 
@@ -12,3 +12,8 @@ class LandListView(ListView):
 class LandDetailView(DetailView):
     model = Land
     template_name = 'land/land_detail.html'
+
+class LandCreateView(CreateView):
+    model = Land
+    template_name = 'land/land_create.html'
+    fields= '__all__'
