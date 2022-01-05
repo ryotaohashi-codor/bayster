fd110715d1e06e05bb8886ac5dcea12a58ce8d90
ryota <ohashi@codor.co.jp>
2022-01-05 16:45:54 +0900

    add land model field and listview layout

--- bayster/land/models.py
+++ bayster/land/models.py
@@ -3,4 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Land(models.Model):
-    title = models.CharField(max_length=40)
+  title = models.CharField(max_length = 40)
+  address = models.TextField()
+  size = models.FloatField()
+  purchase_price = models.IntegerField()
+  estimated_profit = models.IntegerField()
+  cost = models.IntegerField()
+  project_background = models.TextField()
+  applied_date = models.DateField(auto_now=True)
+  STATUS_CHOICE = ((0,'申請中'),(1, '承認'),(2, '却下'))
+  status = models.IntegerField(choices=STATUS_CHOICE, default=0)
+  def __str__(self):
+    return self.title
